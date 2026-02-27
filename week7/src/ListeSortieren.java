5deb01b29b00e7f0a32e8cb4ef73df2f54e49c4c
poinalex01 <poinalex01@gmail.com>
2026-02-27 10:10:09 +0100

    corrected the task requirements

--- week7/src/ListeSortieren.java
+++ week7/src/ListeSortieren.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class ListeSortieren {
     /*
@@ -6,25 +7,21 @@ public class ListeSortieren {
         * Wichtig: Es soll NICHT die eingebaute Sortierfunktion des Listeninterface, sondern euer eigener Algorithmus verwendet werden.
     */
     static void main() {
-        int[] arr = {2,63,21,11,15,18,4,1};
-        System.out.println("arr = " + Arrays.toString(arr));
-        bubbleSort(arr);
-        System.out.println("arr = " + Arrays.toString(arr));
+        List<Integer> list = Arrays.asList(2, 63, 21, 11, 15, 18, 4, 1);
+        System.out.println("list = " + list);
+        bubbleSort(list);
+        System.out.println("list = " + list);
     }
 
-    static void bubbleSort(int[] arr){
+    static void bubbleSort(List<Integer> list) {
         int left, right;
 
-        for (left = 0; left < arr.length - 1; left++) {
-            for (right = 0; right < arr.length - left - 1; right++) {
-                if (arr[right] > arr[right + 1]) {
-                    // int temp = arr[right];
-                    // arr[right] = arr[right + 1];
-                    // arr[right + 1] = temp;
-
-                    arr[right] = arr[right] ^ arr[right + 1];
-                    arr[right + 1] = arr[right] ^ arr[right + 1];
-                    arr[right] = arr[right] ^ arr[right + 1];
+        for (left = 0; left < list.size() - 1; left++) {
+            for (right = 0; right < list.size() - left - 1; right++) {
+                if (list.get(right) > list.get(right + 1)) {
+                    int temp = list.get(right);
+                    list.set(right, list.get(right + 1));
+                    list.set(right + 1, temp);
                 }
             }
         }
